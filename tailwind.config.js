7543b6183de5e74ab4a31b41d05b0af498d72c53
Fitim Maxhuni <fmaxhuni57@gmail.com>
2022-01-04 05:34:13 +0100

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/App.js",
+    "./src/components/ImageSearch.js",
+    "./src/components/ImageCard.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
